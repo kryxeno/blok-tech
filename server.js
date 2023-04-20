e1d7ffded0eb8aaef1fbeadaf11681ef2ca0d959
Mr Bats <timvaningen63@gmail.com>
2023-04-20 13:40:08 +0200

    added ejs templating

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const uri = "mongodb+srv://default:default@blok-tech.lidqbyt.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -13,22 +15,50 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
-    // database and collection code goes here
-    const db = client.db("sample_guides");
-    const coll = db.collection("planets");
-
-    // find code goes here
-    const cursor = coll.find();
-
-    // iterate code goes here
-    await cursor.forEach(console.log);
+    const database = client.db("sample_mflix");
+    const movies = database.collection("movies");
+    // Query for a movie that has the title 'The Room'
+    const query = { title: "De Tim" };
+    const options = {
+      // sort matched documents in descending order by rating
+      sort: { "imdb.rating": -1 },
+      // Include only the `title` and `imdb` fields in the returned document
+      projection: { _id: 0, title: 1, imdb: 1 },
+    };
+    const movie = await movies.findOne(query, options);
+    // since this method returns the matched document, not a cursor, print it directly
+    app.get("/movie", (req, res) => {
+      res.send(movie);
+    });
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
 run().catch(console.dir);
+
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { text: "Boter" });
+});
+
+app.get("/info", (req, res) => {
+  res.send("boter");
+});
+
+app.get("/about", (req, res) => {
+  res.send("boter");
+});
+
+app.get("*", (req, res) => {
+  res.send("what??? 404!!!!!!!!!!!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
