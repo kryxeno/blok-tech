f94ecfd561e3b6b77c5a91d3b81487ba2382384c
Mr Bats <timvaningen63@gmail.com>
2023-04-20 10:59:03 +0200

    connected mongoDB

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
-console.log("kaas");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = "mongodb+srv://default:default@blok-tech.lidqbyt.mongodb.net/?retryWrites=true&w=majority";
 
-const boter = {
-  kaas: "geel",
-  koos: "rood",
-  kees: "blauw",
-};
-
-console.log(boter);
-Object.entries(boter).forEach((e) => {
-  console.log(e);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // database and collection code goes here
+    const db = client.db("sample_guides");
+    const coll = db.collection("planets");
+
+    // find code goes here
+    const cursor = coll.find();
+
+    // iterate code goes here
+    await cursor.forEach(console.log);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
