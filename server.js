cfcba1297e69d1bc00674019d8547e173fe5576f
Mr Bats <timvaningen63@gmail.com>
2023-04-20 10:32:39 +0200

    added server

---
+++ server.js
@@ -0,0 +1,12 @@
+console.log("kaas");
+
+const boter = {
+  kaas: "geel",
+  koos: "rood",
+  kees: "blauw",
+};
+
+console.log(boter);
+Object.entries(boter).forEach((e) => {
+  console.log(e);
+});
